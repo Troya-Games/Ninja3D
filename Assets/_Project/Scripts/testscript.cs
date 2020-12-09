cbef10e15d5c7c970a36157fc4aa53d1713d7fb3
Enescan BEKTAŞ <45511976+wewrere41@users.noreply.github.com>
2020-12-09 22:08:24 +0300

    no message

---
+++ Assets/_Project/Scripts/testscript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testscript : MonoBehaviour
+{
+    private int ste;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
